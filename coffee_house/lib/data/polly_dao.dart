40fa49b7c2d0c8587425655df8d9378b298b3b11
GuilhermeHST <103433937+GuilhermeHST@users.noreply.github.com>
2022-10-28 10:38:15 -0300

    Create polly_dao.dart

---
+++ coffee_house/lib/data/polly_dao.dart
@@ -0,0 +1,23 @@
+import 'package:coffee_house/data/bd_helper.dart';
+import 'package:coffee_house/domain/polly.dart';
+import 'package:sqflite/sqflite.dart';
+
+class PollyDao{
+  Future<List<Polly>> listarCafes() async{
+    DBHelper dbHelper = DBHelper();
+    Database db = await dbHelper.initDB();
+
+    String sql = 'SELECT * FROM polly;';
+    var result = await db.rawQuery(sql);
+    print(sql);
+    
+    List<Polly> lista = <Polly>[];
+    for(var json in result){
+      print(json);
+      Polly cafe = Polly.fromJson(json);
+      lista.add(cafe);
+    }
+    
+    return lista;
+  }
+}
